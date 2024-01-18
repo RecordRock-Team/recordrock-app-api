fc70c8bf8bc0c04e5b0187aaf8110bb4a72e329c
jjhw0106 <jjhw0106@jinhak.com>
2024-01-18 16:58:48 +0900

    Feat: vue 테스트화면이랑 연동 및 RecordRock 캘린더화면 테스트 api 작성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,26 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/test')
   getHello(): string {
+    console.log("들어왔다~");
     return this.appService.getHello();
+  }
+
+  @Get('/calendar')
+  async getCalendar() {
+    const calendarInfo = [
+      {
+        "center_fullname": "더클라임 사당점", 
+        "clear_record": "[ {보라:3}, {남색:4} ]",
+        "daily_memo": "행클했다"
+      },
+      {
+        "center_fullname": "더클라임 사당점", 
+        "clear_record": "[ {보라:3}, {남색:7} ]",
+        "daily_memo": "빵클했다"
+      }
+    ]
+    return calendarInfo;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // CORS를 활성화하는 코드
+  const corsOptions: CorsOptions = {
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    
+  };
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
